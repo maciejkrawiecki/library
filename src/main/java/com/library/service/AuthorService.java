0a3d2926ab5154b9771024317a22954b1d33e4f3
Maciej Krawiecki <m.a.krawiecki@gmail.com>
2019-02-12 22:13:55 +0100

    updated author service

--- src/main/java/com/library/service/AuthorService.java
+++ src/main/java/com/library/service/AuthorService.java
@@ -1,4 +1,32 @@
 package com.library.service;
 
-public class AuthorService {
+import com.library.model.client.AuthorTO;
+import com.library.model.server.Author;
+import com.library.repository.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.library.utils.Converter.convertAuthorTOtoAuthor;
+import static com.library.utils.Converter.convertAuthorsToAuthorTOs;
+
+@Service
+public class
+AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    // mapAuthorTOtoAuthor in utils.Mapper, maps authorTO to author
+    public void createAuthor(AuthorTO authorTO) {
+        Author author = convertAuthorTOtoAuthor(authorTO);
+        authorRepository.save(author);
+    }
+
+    public List<AuthorTO> getAuthors() {
+        return convertAuthorsToAuthorTOs(authorRepository.findAll());
+    }
 }
